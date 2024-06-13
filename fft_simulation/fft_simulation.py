66dbd3f1ec48e260ceb1f2b7d44bd0226936abeb
charles pageot <pageotcharles@gmail.com>
2024-06-13 15:21:12 -0400

    added comments and docstrings to the main functions

--- fft_simulation/fft_simulation.py
+++ fft_simulation/fft_simulation.py
@@ -3,21 +3,44 @@ import nibabel as nib
 import argparse
 
 def is_nifti(filepath):
+    """
+    Check if a file is in the NIfTI format.
+
+    Arguments:
+        filepath (str): the filepath to check
+
+    Returns:
+        bool: True if the file is NifTI, False otherwise.
+    """
     if filepath[-4:] == '.nii':
         return True
     else:
         return False
 
 def load_sus_dist(filepath):
+    """
+    Load the susceptibility data and the image resolution from a NiFTI file.
+
+    Arguments:
+        filepath (str): the file containing the susceptibility distribution
+
+    Returns:
+        (sus_dist, img_res): A tuple containing the susceptibility data (np.array)
+                            and the image resolution (np.array)
+    """    
     img = nib.load(filepath)
-    data = img.get_fdata()
+    sus_dist = img.get_fdata()
     header = img.header
     img_res = np.array(header.get_zooms())
 
-    return data, img_res
+    return sus_dist, img_res
 
 def compute_bz(sus_dist, img_res=np.array([1,1,1]), buffer=1):
-
+    """
+    Compute the Bz field variation based on a susceptibility distribution
+    using a Fourier-based method.
+    
+    """
     # dimensions needs to be a numpy.array
     dimensions = np.array(sus_dist.shape)
 
@@ -32,8 +55,11 @@ def compute_bz(sus_dist, img_res=np.array([1,1,1]), buffer=1):
 
     # FFT kernel
     k2 = kx**2 + ky**2 + kz**2
-    kernel = np.fft.fftshift(1/3 - kz**2/k2)
-    kernel[0,0,0] = 1/3
+
+    # undetermined at the center of k-space
+    with np.errstate(divide='ignore', invalid='ignore'):
+        kernel = np.fft.fftshift(1/3 - kz**2/k2)
+        kernel[0,0,0] = 1/3
 
     FT_chi = np.fft.fftn(sus_dist, dim)
     Bz_fft = kernel*FT_chi
@@ -44,12 +70,24 @@ def compute_bz(sus_dist, img_res=np.array([1,1,1]), buffer=1):
 
     return volume_without_buff
 
-def save_to_nifti(sus_dist, img_res, output_path):
+def save_to_nifti(data, img_res, output_path):
+    """
+    Save data to NIfTI format to a specified output path.
+
+    Arguments:
+        data (np.array): the data to save
+        img_res (np.array): the spatial resolution of the data
+        output_path (str): the output path to save the file
+
+    Returns:
+        None
+    """
     affine = np.diag(np.append(img_res,1))
-    nifti_img = nib.Nifti1Image(sus_dist, affine)
+    nifti_img = nib.Nifti1Image(data, affine)
     nib.save(nifti_img, output_path)
 
 def main():
+
     parser = argparse.ArgumentParser()
     parser.add_argument("-i",
                         dest="input_file",
