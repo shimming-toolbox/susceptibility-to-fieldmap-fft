f9ccc87940cb349aeff1e12ed4680364ca3715f8
charles pageot <pageotcharles@gmail.com>
2024-07-05 10:47:30 -0400

    Update setup.py file to include entry points

--- setup.py
+++ setup.py
@@ -1,6 +1,30 @@
 from setuptools import find_packages, setup
+from os import path
+
+# Get the directory where this current file is saved
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, "README.rst"), encoding="utf-8") as f:
+    long_description = f.read()
 
 setup(
-    name="fft_simulation",
-    packages=find_packages(),
+    name="susceptibility-to-fieldmap-fft",
+    python_requires=">=3.8.8",
+    description="Code to compute the magnetic field variation from a given susceptibility distribution placed in a constant magnetic field.",
+    long_description=long_description,
+    url="https://github.com/shimming-toolbox/susceptibility-to-fieldmap-fft",
+    entry_points={
+        'console_scripts': [
+            "compute_fieldmap=fft_simulation.fft_simulation:main",
+            "analytical_cases=fft_simulation.analytical_cases:main"
+        ]
+    },
+    packages=find_packages(exclude=["docs"]),
+    install_requires=[
+        "click",
+        "numpy>=1.24.4",
+        "nibabel>=5.2.1",
+        "matplotlib>=3.7.5",
+        "scipy>=1.10.1"
+    ],
 )
